abd07350148f15b63d508a9b0da2f3a3adcbacd7
nksound <NKSound@mail.ru>
2024-11-07 14:44:40 +0300

    "Хендлеры обработки сообщений"

--- module_13_2.py
+++ module_13_2.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-api = "7374724689:AAE7FraX7jq8h1vuOPvZTo01AZGeN5EBNPQ"
+api = "..."
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 

404aa4179fda0d3ac13fcd9ba15b1dcbf4b0bcae
nksound <NKSound@mail.ru>
2024-11-13 20:21:23 +0300

    "Инлайн клавиатуры"

--- module_13_6.py
+++ module_13_6.py
@@ -4,30 +4,35 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-api = "..."
+api = "7374724689:AAE7FraX7jq8h1vuOPvZTo01AZGeN5EBNPQ"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-# kb = ReplyKeyboardMarkup(resize_keyboard=True)
-# button_calc = KeyboardButton(text='Рассчитать')
-# button_info = KeyboardButton(text='Информация')
-# kb.add(button_calc)
-# kb.add(button_info)
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button_calc = KeyboardButton(text='Рассчитать')
+button_info = KeyboardButton(text='Информация')
+kb.add(button_calc)
+kb.add(button_info)
 inline_keyboard = InlineKeyboardMarkup(row_width=1)
+male_keyboard = InlineKeyboardMarkup(row_width=1)
 button_calories = InlineKeyboardButton("Рассчитать норму калорий", callback_data='calories')
 button_formulas = InlineKeyboardButton("Формулы расчёта", callback_data='formulas')
+button_male = InlineKeyboardButton("Мужской", callback_data='male')
+button_female = InlineKeyboardButton("Женский", callback_data='female')
 inline_keyboard.add(button_calories, button_formulas)
+male_keyboard.add(button_male, button_female)
 
 
 @dp.message_handler(commands=['start'])
 async def start(message):
-    await message.answer('Привет! Я бот, помогающий твоему здоровью.', reply_markup=inline_keyboard)
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.', reply_markup=kb)
 
 
 class UserState(StatesGroup):
     age = State()
     growth = State()
     weight = State()
+    gender = State()
 
 
 @dp.message_handler(text='Рассчитать')
@@ -47,7 +52,21 @@ async def get_formulas(call):
 
 
 @dp.callback_query_handler(text='calories')
-async def set_age(call: types.CallbackQuery):
+async def set_gender(call: types.CallbackQuery):
+    await call.message.answer('Выберите ваш пол:', reply_markup=male_keyboard)
+    await UserState.gender.set()
+
+
+@dp.callback_query_handler(text='male', state=UserState.gender)
+async def male_selected(call: types.CallbackQuery, state: FSMContext):
+    await state.update_data(gender='male')
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.callback_query_handler(text='female', state=UserState.gender)
+async def female_selected(call: types.CallbackQuery, state: FSMContext):
+    await state.update_data(gender='female')
     await call.message.answer('Введите свой возраст:')
     await UserState.age.set()
 
@@ -73,8 +92,15 @@ async def send_calories(message, state: FSMContext):
     age = int(data['age'])
     growth = int(data['growth'])
     weight = int(data['weight'])
-    bmr = 10 * weight + 6.25 * growth - 5 * age + 5
-    # Формула Миффлина - Сан Жеора для расчета калорий (применим для мужчин)
+    gender = data['gender']
+
+    if gender == 'male':
+        bmr = 10 * weight + 6.25 * growth - 5 * age + 5
+        # Формула Миффлина - Сан Жеора для расчета калорий (применим для мужчин)
+    else:
+        bmr = 10 * weight + 6.25 * growth - 5 * age - 161
+        # Формула Миффлина - Сан Жеора для расчета калорий (применим для женщин)
+
     await message.answer(f'Ваша норма калорий: {bmr:.2f} ккал в день.')
     await state.finish()
 

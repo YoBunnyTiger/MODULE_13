fc957bda3fa18c7733ef0d587e3985408348c349
nksound <NKSound@mail.ru>
2024-11-13 20:23:16 +0300

    "Инлайн клавиатуры"

--- module_13_6.py
+++ module_13_6.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-api = "7374724689:AAE7FraX7jq8h1vuOPvZTo01AZGeN5EBNPQ"
+api = "..."
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
